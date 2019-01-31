d2f1e9be178116785fce1dbd822e6c086f62f1f0
Sarah906 <35878046+Sarah906@users.noreply.github.com>
2019-01-31 15:58:37 +0300

    Add files via upload

--- Sarcasm_Detection1.py
+++ Sarcasm_Detection1.py
@@ -25,71 +25,22 @@ from collections import Counter
 
 # To stop words
 import nltk
+
 from nltk.corpus import stopwords
 stop = stopwords.words('english')
 
-# ## Load the data
 
-# In[2]:
+import pickle
 
-
-data = pd.read_json('Sarcasm_Headlines_Dataset.json',lines=True)
-
-
-# ## Vectorize headlines
-
-# In[3]:
-
-
-# Create vectorizer
-countVectorizer = CountVectorizer(stop_words=stop)
-
-# Vectorize text
-vectorizedText = countVectorizer.fit_transform(data['headline'].str.replace("'", '').values)
-
-
-# In[7]:
-
-
-vec = pd.DataFrame(vectorizedText.toarray(),columns=countVectorizer.get_feature_names())
-
-
-# In[8]:
-
-
-NLP = vec
-
-
-# ## Train split
-
-# In[9]:
-
-
-X = NLP
-y = data.is_sarcastic
-
-
-# In[11]:
-
-
-# In[12]:
-
-
-# ## LogisticRegression
-
-# In[ ]:
-
-
-# import the class
-from sklearn.linear_model import LogisticRegression
-
-# In[ ]:
-
-
-# instantiate the model
-logreg = LogisticRegression()
-# fit the model to the data
-logreg.fit(X,y)
+with open('data.pickle', 'rb') as f:
+    # The protocol version used is detected automatically, so we do not
+    # have to specify it.
+    logreg = pickle.load(f)
+    
+with open('counter.pickle', 'rb') as f:
+    # The protocol version used is detected automatically, so we do not
+    # have to specify it.
+    vectorizer = pickle.load(f)
 
 #### Farah Part ==========================
 ## Building Flask Enviroment
@@ -105,7 +56,7 @@ app = Flask(__name__)
 @app.route('/predict',methods = ["GET", "POST"])
 def hello_predict():
     text = request.args.get("text")
-    test_text = countVectorizer.transform([text.replace("'", '')])
+    test_text = vectorizer.transform([text.replace("'", '')])
     class_ = int(logreg.predict(test_text)[0])
     class_prob = logreg.predict_proba(test_text)
     class_prob_0 = float(class_prob[0][0])
@@ -113,4 +64,9 @@ def hello_predict():
     
     result = {"is_sarcasm": class_,'probability_0': class_prob_0, 'probability_1': class_prob_1 }
     return jsonify(result)
+
+
+if __name__ == '__main__':
+    app.run()
+
 
