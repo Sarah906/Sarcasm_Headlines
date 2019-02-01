dcd9e3e546ea3852ccb0bf5d7db7d13c1fa989f3
Sarah906 <35878046+Sarah906@users.noreply.github.com>
2019-02-01 21:55:46 +0300

    Add files via upload

--- Sarcasm_Detection1.py
+++ Sarcasm_Detection1.py
@@ -58,7 +58,10 @@ def hello_predict():
     return jsonify(result)
 
 
-if __name__ == '__main__':
-app.run(host='0.0.0.0', port=80)
 
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host = '0.0.0.0',port=80)
 
